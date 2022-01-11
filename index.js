89ad2d07471d3343b16947cde5008d0ebc1cd6e2
Eric Einerson <eaeiner.gmail.co>
2022-01-11 09:30:42 -0700

    commit tree abd20ef334c915d5a7be533b8295390ff6e7dc44

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(number){
   // Write your code here!
+  if (number===199){
+    return 'This one is on me!';
+  }
+  else if (number===2001){
+    return 'I will gladly take your thirty bucks.';
+  }
+  else if (number>2500){
+    return 'No can do.'
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  city == "NYC" ? 'Ok, sounds good.' : 'No go.'
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(number3){
   // Write your code here!
 }
